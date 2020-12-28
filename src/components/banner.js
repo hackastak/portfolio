ab6e11ba6f31daada3cff4526b3ffacc2a782d67
DevWig <ahwig93@gmail.com>
2020-12-28 10:23:04 -0600

    Add banner component and styles

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
+//Import Icons
+import { FaInstagramSquare } from 'react-icons/fa';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaTwitterSquare } from 'react-icons/fa';
+
 import '../styles/css-reset.css';
 import '../styles/banner-styles.scss';
 
@@ -23,11 +28,19 @@ const Banner = () => {
     <div className="banner-container">
       <div className="image-container">
         <Img className="top-center-img"fluid={data.profileImg.childImageSharp.fluid} />
-        
-        
       </div>
       <div className="blurb-container">
-        <p className="blurb">Hi, my name is Hunter Wiginton, and I am a web developer and graphic designer. </p>
+        <p>Hi, my name is Hunter Wiginton.</p>
+        <p>I am a web developer and graphic designer. </p>
+        <p>Have an idea?</p>
+        <button>Let's talk!</button>
+        <div className="social-link-container">
+          <a className="social-icon" target="_blank" href="https://www.instagram.com/dev.wig/"><FaInstagramSquare /></a>
+          <a className="social-icon" target="_blank" href="https://www.facebook.com/hunter.wiginton.3/"><FaFacebookSquare /></a>
+          <a className="social-icon" target="_blank" href="https://www.facebook.com/hunter.wiginton.3/"><FaTwitterSquare /></a>
+        </div>
+        
+        
       </div>
 
     </div>
