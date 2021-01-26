3f47aa3bbd5dcd403696a2f6716c60f919c4a14a
DevWig <ahwig93@gmail.com>
2021-01-26 09:53:17 -0600

    Add siteUrl key to gatsby-config.js to squash bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Dev.Wig Portfolio'
+    title: 'Dev.Wig Portfolio',
+    siteUrl: 'https://devwig.com'
   },
   plugins: [
     "gatsby-plugin-postcss",
@@ -21,32 +22,32 @@ module.exports = {
         display: "swap"
       },
     },
-    {
-      resolve: "gatsby-plugin-prefetch-google-fonts",
-      options: {
-        fonts: [
-          {
-            family: "Ubuntu",
-            variants: ["300", "400", "400i", "700"]
-          },
-          {
-            family: "Spectral",
-            variants: ["400", "700", "700i", "800"]
-          },
-          {
-            family: "Pacifico"
-          }
-        ],
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-prefetch-google-fonts",
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: "Ubuntu",
+    //         variants: ["300", "400", "400i", "700"]
+    //       },
+    //       {
+    //         family: "Spectral",
+    //         variants: ["400", "700", "700i", "800"]
+    //       },
+    //       {
+    //         family: "Pacifico"
+    //       }
+    //     ],
+    //   },
+    // },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
+        name: "Dev.Wig Portfolio",
+        short_name: "Portfolio",
         start_url: "/",
         background_color: "#f7f0eb",
         theme_color: "#a2466c",
